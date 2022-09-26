210b9a3bb6041ed04ab93bac6a94a13406b2eead
Sebastian Ramirez <sebasramirez.dev@gmail.com>
2022-09-26 01:27:09 -0500

    fields checkout

---
+++ pages/checkout/index.jsx
@@ -0,0 +1,73 @@
+const index = () => {
+  return (
+    <div className="container">
+      <div className="checkoutInfo">
+        <h1>Finalizar compra</h1>
+
+        <form action="https://formbold.com/s/FORM_ID" method="POST">
+          <div className="-mx-3 flex flex-wrap">
+            <div className="w-full px-3 sm:w-1/2">
+              <div className="mb-5">
+                <label
+                  htmlhtmlFor="fName"
+                  className="mb-3 block text-base font-medium text-[#07074D]"
+                >
+                  First Name
+                </label>
+                <input
+                  type="text"
+                  name="fName"
+                  id="fName"
+                  placeholder="First Name"
+                  className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                />
+              </div>
+            </div>
+            <div className="w-full px-3 sm:w-1/2">
+              <div className="mb-5">
+                <label
+                  htmlFor="lName"
+                  className="mb-3 block text-base font-medium text-[#07074D]"
+                >
+                  Last Name
+                </label>
+                <input
+                  type="text"
+                  name="lName"
+                  id="lName"
+                  placeholder="Last Name"
+                  className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="mb-5">
+            <label
+              htmlFor="guest"
+              className="mb-3 block text-base font-medium text-[#07074D]"
+            >
+              How many guest are you bringing?
+            </label>
+            <input
+              type="number"
+              name="guest"
+              id="guest"
+              placeholder="5"
+              min="0"
+              className="w-full appearance-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+            />
+          </div>nnb
+
+          <div>
+            <button className="hover:shadow-form rounded-md bg-[#6A64F1] py-3 px-8 text-center text-base font-semibold text-white outline-none">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className="checkoutSumary"></div>
+    </div>
+  );
+};
+
+export default index;

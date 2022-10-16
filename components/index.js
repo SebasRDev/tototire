8c18ec3018fa6928c0ea5336f90bbf1720c9c359
Sebastian Ramirez <sebasramirez.dev@gmail.com>
2022-10-15 19:32:42 -0500

    add component

--- components/index.js
+++ components/index.js
@@ -5,13 +5,10 @@ export * from './navbar/Cart';
 export * from './navbar/Navbar';
 export * from './product/DetailSlider';
 export * from './product/TableData';
-<<<<<<< HEAD
 export * from './services/ServiceSlide';
 export * from './shared/Button';
 export * from './shared/FeaturedContent';
 export * from './shared/FeaturedSlider';
 export * from './shared/ProductCard';
-=======
 export * from './product/DetailSlider';
 export * from './product/DetailMagnifier';
->>>>>>> fcadab78dfe406ee32e43c0b7e62e4250c892589
